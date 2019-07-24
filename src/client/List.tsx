e7d581106a919245e2af82c07ccfd0dad012fe21
Dexx <dexx.van.der.mandele@mendix.com>
2019-07-24 13:10:25 +0200

    Add empty list message

--- src/client/List.tsx
+++ src/client/List.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 import Spell from "../spells/Spell";
 
@@ -17,6 +18,22 @@ export default class List extends React.Component<IProps> {
     }
 
     private renderItems() {
-        return this.props.items.map(item => this.props.renderItem(item));
+        const items = this.props.items;
+        if (items.length === 0) {
+            return (
+                <View style={styles.emptyListMessage}>
+                    <Text>You have no spells in this category</Text>
+                </View>
+            );
+        }
+        return items.map(item => this.props.renderItem(item));
     }
 }
+
+const styles = StyleSheet.create({
+    emptyListMessage: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
